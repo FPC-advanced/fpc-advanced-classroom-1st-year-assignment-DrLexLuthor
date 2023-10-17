1c5dea10c6c4707b64ccfacea1c701ce8a2850b4
Your Name <you@example.com>
2023-10-17 12:06:52 +0530

    committed 10/17/2023, 12:06:52 PM

--- set01/problem03.c
+++ set01/problem03.c
@@ -6,20 +6,41 @@ void output(int a, int b, int sum);
 
 int input(){
 
-printf()
+int numb;
 
-
-
+scanf("%d",&numb);  
+return numb;
 }
 
 int add(int a, int b){
+int sum;
+sum=0;
+sum=a+b;
+}
+
+void output(int a, int b, int sum)
+{
+
+printf("the sum is %d",sum);
+
+}
 
 
 
+int main() {
+    int num1, num2, sum;
+
+    printf("enter first number: ");
+    num1 = input();
+
+    printf("nter second number: ");
+    num2 = input();
+
+    sum = add(num1, num2);
+
+    output(num1, num2, sum);
+
+    return 0;
 
 
-
-
-
-    
 }
