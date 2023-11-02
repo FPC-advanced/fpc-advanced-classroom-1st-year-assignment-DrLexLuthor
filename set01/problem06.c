ceda5ebbe6ea63e0ab6610e3db95dd1e4c28c0bc
Your Name <you@example.com>
2023-11-02 12:13:23 +0530

    committed 11/2/2023, 12:13:22 PM

--- set01/problem06.c
+++ set01/problem06.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int input(int *a, int *b, int *c);
-void compare(int a, int b, int c,int g, int *largest);
+void compare(int a, int b, int c,int *g, int *largest);
 void output(int g,int largest);
 int input(int *a,int *b ,int *c){
    
@@ -9,19 +9,19 @@ int input(int *a,int *b ,int *c){
    
 }
 
-void compare(int a, int b, int c,int g, int *largest){
+void compare(int a, int b, int c,int *g, int *largest){
 
  
  if(a>b&&a>c)
-    {*largest=a;g=0;}
+    {*largest=a;*g=0;}
    else if(b>a&&b>c){
-        *largest =b;g=0;
+        *largest =b;*g=0;
     }
   else if(c>a&&c>b){
-        *largest=c;g=0;
+        *largest=c;*g=0;
     }
     else{
-   g=1;
+   *g=1;
        }
     }
 
@@ -34,10 +34,10 @@ void output(int g,int largest){
 }
 
 int main(){
-    int a,b,c,lg,r;
+    int a,b,c,largest,g;
     printf("enter 3 numbers");
-  inp(&a,&b,&c);
- check(a,b,c,&r,&lg);
- out(lg,r);
+  input(&a,&b,&c);
+ compare(a,b,c,&g,&largest);
+ output(largest,g);
     return 0;
     }
