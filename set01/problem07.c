83a7c165892793b84a4c96db56baabe13367553d
Your Name <you@example.com>
2023-11-06 10:11:14 +0530

    committed 11/6/2023, 10:11:13 AM

---
+++ set01/problem07.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int input_n();
+int sum_n_nos(int n);
+void output(int sum);
+int input_n(){
+
+    int a;
+    printf("input number");
+
+    scanf("%d",&a);
+
+    return a;
+
+}
+
+int sum_n_nos(int n){
+
+    int sum=0;
+    for(int i=1;i<n;i++){
+
+        sum+=i;
+    }
+    return sum;
+}
+
+void output(int sum){
+
+    printf("the sum of numbers form 1 to the given number is%d",sum);
+
+}
+int main(){
+    int n,sum;
+
+    n = input_n();
+
+    sum =sum_n_nos(n);
+
+    output(n, sum);
+
+    return 0;
+}
--- set02/problem01.c
+++ set02/problem01.c
@@ -2,14 +2,43 @@
 
 void input(float *base, float *height);
 void find_area(float base , float height, float *area);
-void output(float base, float height, float area);
+void output(float area);
 
 void input(float *base, float *height){
 
+printf("type the base of the triangal \n");
+scanf("%f",base);
 
-
+printf("enter the height of the triangal\n");
+scanf("%f",height);
 
 }
 
- 
-  
+void find_area(float base , float height, float *area){
+
+*area=0.5*base*height;
+
+}
+void output(float area){
+
+printf("area=%.2f\n",area);
+
+}
+
+int main(){
+
+float base, height, area;
+
+
+input(&base,&height);
+
+find_area(base,height,&area);
+
+output(area);
+
+
+
+
+
+    return 0;
+}
