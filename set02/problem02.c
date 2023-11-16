64519029ae4deb0cdf28b10b6a247a2c68d1e279
Your Name <you@example.com>
2023-11-16 11:49:44 +0530

    committed 11/16/2023, 11:49:44 AM

--- set02/problem02.c
+++ set02/problem02.c
@@ -2,6 +2,25 @@
 
 int input_side();
 int check_scalene(int a, int b, int c);
-void output(int a, int b, int c, int isscalene);
+void output(int isscalene);
 
-
+#include<stdio.h>
+int input_side(){
+    int s;
+    printf("enter a side\n");
+   return (scanf("%d",&s));
+}
+int check_scalene(int a, int b, int c){
+    if(a==b&&a==c) return 1;
+    else return 0;
+}
+void output(int isscalene){
+    if(isscalene)printf("scalene\n");
+    else printf("not scalene\n");
+}
+int main(){
+    int a,b,c,res;
+    a = input_side(); b = input_side(); c = input_side();
+    output(res);
+    return 0;
+}
