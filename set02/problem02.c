1026a9965d2cece9c36b900ba25cb24d9dfaefda
Your Name <you@example.com>
2023-12-04 18:48:07 +0530

    committed 12/4/2023, 6:48:06 PM

--- set02/problem02.c
+++ set02/problem02.c
@@ -8,19 +8,45 @@ void output(int isscalene);
 int input_side(){
     int s;
     printf("enter a side\n");
-   return (scanf("%d",&s));
+    scanf("%d",&s);
+    return s;
 }
 int check_scalene(int a, int b, int c){
-    if(a==b&&a==c) return 1;
-    else return 0;
+  
+if ( (a!=b ) && (b!=c)&& (c!=a) )
+
+{
+    return 1;
 }
+    else
+{
+    return 0;
+}
+}
+
 void output(int isscalene){
-    if(isscalene)printf("scalene\n");
-    else printf("not scalene\n");
+   printf("the triangal with the given sides is ");
+
+   if(isscalene==1)
+   {
+    printf("scalene\n");
+   }
+else if (isscalene==0){
+
+    printf("not scalene");
 }
+}
+
 int main(){
     int a,b,c,res;
-    a = input_side(); b = input_side(); c = input_side();
-    output(res);
-    return 0;
+    a = input_side();
+     b = input_side();
+      c = input_side();
+   
+res =check_scalene(a,b,c);
+
+output(res);
+
+return 0;
+
 }
---
+++ set02/problem03.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int input_number();
+int is_composite(int n);
+void output(int n, int result);
+
+int input_number(){int n;
+    printf("enter a number");
+    scanf("%d",&n);
+    return n;
+}
+int is_composite(int n){
+    int i ;
+    for(i=0;i<=11;i++){
+    if (n%i==0&&n!=3&&n!=2&&n!=5&&n!=7&&n!=11){
+        i = 12;
+        return 1;
+    }
+    else{
+        return 0;
+    }
+    }
+}
+void output(int n, int result){
+    if(result)
+    printf("%d is composite",n);
+    else{printf("not composite");}
+}
+
+int main(){
+    int n,res;
+    n = input_number();
+
+    res = is_composite(n);
+
+    output(n,res);
+    
+    return 0;
+}
